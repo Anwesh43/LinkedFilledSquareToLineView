dffd052a3828057b8cced668f6bbb09274d16cff
Anwesh43 <anweshthecool0@gmail.com>
2019-09-18 10:08:09 +0530

    adding constants

--- filledsquaretolineview/src/main/java/com/anwesh/uiprojects/filledsquaretolineview/FilledSquareToLineView.kt
+++ filledsquaretolineview/src/main/java/com/anwesh/uiprojects/filledsquaretolineview/FilledSquareToLineView.kt
@@ -6,3 +6,17 @@ package com.anwesh.uiprojects.filledsquaretolineview
 
 import android.view.View
 import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.01f
+val color1 : Int = Color.parseColor("#9C27B0")
+val color2 : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 25
